dfa4944781761c9e3a2ea752924d66b7d2e76282
Jenna Martin <jennamar828@gmail.com>
2023-09-27 19:40:34 +0000

    created element

--- js/main.js
+++ js/main.js
@@ -38,17 +38,30 @@ call to axios npm
 // beginning api call 
 const apiKey = "67a57437454438749ceaaa4b9d41767b";
 const api_url = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiKey}`;
-function init(){
     //setup variables, const
     // clickhandlers
     //
-    console.log("hello sandbox");
-    test_axios();
-};
+
 
 async function test_axios(){
-    const result = await axios.get(api_url);
+    const result = axios.get(api_url);
     console.log(result);
 };
 
-// create display box, and everything that will be contained inside it
+document.body.onload = addElement;
+// create display box(div element), and everything that will be contained inside it
+function addElement() {
+    const newDiv = document.createElement("div");
+
+    // give it some content
+    const newContent = document.createTextNode("this is some content!");
+
+    //add the text node to the newly created div
+    newDiv.appendChild(newContent);
+
+    // add the newly created element and its content into the DOM
+    const currentDiv = document.getElementById("main");
+    document.body.insertBefore(newDiv, currentDiv);
+
+
+}
