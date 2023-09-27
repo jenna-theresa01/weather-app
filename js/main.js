a1488f8b81bb47897970f3a093100bc10db1d643
Jenna Martin <jennamar828@gmail.com>
2023-09-27 18:39:27 +0000

    got api working in console

--- js/main.js
+++ js/main.js
@@ -10,9 +10,10 @@ addEventistener()
     
          
 async/await
-append
+appendChild
 
 local storage?? 
+    window.localStorage - stores data with no expiration date
 conversions for celcius and kelvin? 
 input zip box
     make this required
@@ -20,15 +21,34 @@ input zip box
         top 5 matches?
 
 "get weather" button
+    document.createElement()
 display box
     city
     temp (do I need conversions here?)
     condition
     other info (image)
 message for unsuccessful search "invalid zip code"
-
-
-
+call to axios npm 
 
 
 */ 
+
+// testing axios call
+// global vars
+// beginning api call 
+const apiKey = "67a57437454438749ceaaa4b9d41767b";
+const api_url = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiKey}`;
+function init(){
+    //setup variables, const
+    // clickhandlers
+    //
+    console.log("hello sandbox");
+    test_axios();
+};
+
+async function test_axios(){
+    const result = await axios.get(api_url);
+    console.log(result);
+};
+
+// create display box, and everything that will be contained inside it
